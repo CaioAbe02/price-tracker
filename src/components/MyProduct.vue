b31ef711d8472907e4631386e0e4ce32fef65dac
CaioAbe02 <caioabe68@gmail.com>
2023-08-08 16:58:25 -0300

    add: myproduct page

---
+++ src/components/MyProduct.vue
@@ -0,0 +1,94 @@
+<template v-if="product">
+    <div class="product_card">
+        <div class="discount_icon">
+            <font-awesome
+                icon="fa-solid fa-percent"
+                size=lg
+                border
+                style="--fa-border-radius: 100%; --fa-border-width: 3px;"
+                :style="{ '--fa-border-color': getColor(), color: getColor() }"
+            />
+        </div>
+        <span class="product_name">{{ product.name }}</span>
+        <span class="product_price" :style="{ color: getColor() }">R${{ product.new_prices.slice(-1)[0].toFixed(2) }}</span>
+        <span class="product_discount" :style="{ color: getColor() }">{{ getDiscount() }}</span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MyProduct',
+    props: {
+        product: {
+            required: true,
+            type: Object
+        }
+    },
+    methods: {
+        getDiscount() {
+            const new_price = this.product.new_prices.slice(-1)[0]
+            const original_price = this.product.original_price
+            const discount = (100 - (original_price / new_price) * 100).toFixed(2)
+
+            if (new_price > original_price) {
+                return (`+${discount}%`)
+            }
+            else if (new_price < original_price) {
+                return (`${discount}%`)
+            }
+            else {
+                return "0%"
+            }
+        },
+        getColor() {
+            const difference = this.product.new_prices.slice(-1) - this.product.original_price
+
+            if (difference > 0) {
+                return '#D82E3F'
+            }
+            else if (difference < 0) {
+                return '#28CC2D'
+            }
+            else {
+                return '#FFE135'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.product_card {
+    display: grid;
+    grid-template-areas:
+        'icon name name'
+        'icon price discount';
+    grid-template-columns: auto 120px auto;
+    justify-content: left;
+    align-items: center;
+
+    padding-top: 10px;
+    padding-left: 120px;
+}
+
+.discount_icon {
+    grid-area: icon;
+    padding-right: 15px;
+}
+
+.product_name {
+    grid-area: name;
+
+    color: white;
+    font-weight: bold;
+}
+
+.product_price {
+    grid-area: price;
+}
+
+.product_discount {
+    grid-area: discount;
+}
+</style>
