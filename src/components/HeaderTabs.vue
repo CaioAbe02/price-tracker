0d7e5896b4d6715c78157bfb9611b5d65c1a0bd9
CaioAbe02 <caioabe68@gmail.com>
2023-08-08 16:57:34 -0300

    add: header

---
+++ src/components/HeaderTabs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="tabs">
+        <button class="tab">My products</button>
+        <button class="tab">Products</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderTabs'
+}
+</script>
+
+<style scoped>
+.tabs {
+    height: 50px;
+    background-color: blue;
+
+    display: flex;
+    justify-content: center;
+}
+</style>
