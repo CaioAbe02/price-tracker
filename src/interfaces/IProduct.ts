de26fe46298ce3001c8d6295a73f23f28fd50546
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2023-08-11 10:56:16 -0300

    ✨feat: add IProduct interface

---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+export default interface IProduct {
+    id: number,
+    name: string,
+    new_prices: number[],
+    new_prices_dates: string[],
+    original_price: number,
+    tags: string,
+    url: string
+}
