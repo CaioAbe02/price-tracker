824be807b853a8cac7d86ee738aafbb1a36716d4
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2023-08-11 10:59:04 -0300

    ✨feat: add AddProduct page

---
+++ src/views/AddProduct.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Add product</h1>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'AddProduct',
+})
+</script>
+
+<style scoped>
+h1 {
+    color: white;
+}
+</style>
