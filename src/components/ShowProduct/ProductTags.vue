83dba2e3d38fda87ddc33942ed1456127af0ce5a
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2023-08-23 13:50:48 -0300

    ✨feat: add product tags component

---
+++ src/components/ShowProduct/ProductTags.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="tagsContainer">
+        <span v-for="tag in tagsArray" :key="tag" class="tag">{{ tag }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ProductTags',
+    props: {
+        tags: {
+            required: true,
+            type: String
+        },
+    },
+    computed: {
+        tagsArray() {
+            return this.tags.split(",").map(tag => tag.trim())
+        }
+    }
+})
+</script>
+
+<style scoped>
+.tagsContainer {
+    display: flex;
+    flex-direction: row;
+}
+
+.tag {
+    color: white;
+    font-size: 0.8rem;
+
+    background-color: #434658;
+    border-radius: 5px;
+
+    padding: 3px 5px;
+    margin-right: 7px;
+}
+</style>
