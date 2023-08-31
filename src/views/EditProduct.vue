212308a56d92ec032fff47f70e8be0767f8f3670
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2023-08-30 23:18:46 -0300

    🎨css: style forms

--- src/views/EditProduct.vue
+++ src/views/EditProduct.vue
@@ -1,18 +1,18 @@
 <template>
-    <section>
+    <section class="card">
+        <h1>Edit product</h1>
         <form @submit.prevent="editProduct()">
-            <div>
+            <div class="input_group">
+                <label for="editedProduct.name">Name</label>
                 <input type="text" v-model="editedProduct.name">
-                <label for="editedProduct.tags">
-                    Tags
-                    <input type="text" v-model="editedProduct.tags">
-                </label>
             </div>
-            <div>
-                <button type="submit">
-                    Edit product
-                </button>
+            <div class="input_group">
+                <label for="editedProduct.tags">Tags</label>
+                <input type="text" v-model="editedProduct.tags">
             </div>
+            <button type="submit">
+                Submit
+            </button>
         </form>
     </section>
 </template>
@@ -72,4 +72,70 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+    background-color: var(--card-background);
+    max-width: 500px;
+    border-radius: 10px;
+
+    margin: 0 auto;
+    padding: 40px;
+}
+
+form {
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+}
+
+h1 {
+    font-size: 1.5rem;
+    color: white;
+    text-align: center;
+}
+
+label {
+    display: block;
+    margin-bottom: 5px;
+
+    color: var(--input-label);
+}
+
+.input_group {
+    margin-top: 5px;
+}
+
+input {
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    box-sizing: border-box;
+    background-color: var(--card-background);
+    border: 1px solid var(--input-border);
+    border-radius: 5px;
+    outline: 0;
+
+    padding: 10px 15px;
+    margin-top: 2.5px;
+
+    color: white;
+}
+
+input:focus {
+    border: 1px solid var(--purple);
+}
+
+button {
+    align-self: center;
+    background-color: var(--purple);
+
+    padding: 7px 15px;
+    border-width: 0px;
+    border-radius: 5px;
+
+    margin-top: 40px;
+
+    cursor: pointer;
+}
+</style>
