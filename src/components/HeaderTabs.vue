8e35d58cdc13a58d3e56774b44337456fa6932fa
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2023-11-04 00:15:00 -0300

    🎨css: active tabs with different style

--- src/components/HeaderTabs.vue
+++ src/components/HeaderTabs.vue
@@ -1,22 +1,82 @@
 <template>
     <div class="tabs">
-        <router-link to="/" class="link">My Products</router-link>
-        <router-link to="/products" class="link">Products</router-link>
-        <router-link to="/products/update" class="link">Update products</router-link>
+        <router-link
+            to="/"
+            class="tab"
+            :class="isActive('favoriteProducts')"
+            @click="setActivetab('favoriteProducts')"
+            @mouseover="mouseOver('favoriteProducts')"
+            @mouseleave="mouseLeave()"
+            @focus="mouseOver('favoriteProducts')"
+            @focusout="mouseLeave()"
+        >
+            Favorite Products
+        </router-link>
+        <router-link
+            to="/products"
+            class="tab"
+            :class="isActive('products')"
+            @click="setActivetab('products')"
+            @mouseover="mouseOver('products')"
+            @mouseleave="mouseLeave()"
+            @focus="mouseOver('products')"
+            @focusout="mouseLeave()"
+        >
+            Products
+        </router-link>
+        <router-link
+            to="/products/update"
+            class="tab"
+            :class="isActive('updateProducts')"
+            @click="setActivetab('updateProducts')"
+            @mouseover="mouseOver('updateProducts')"
+            @mouseleave="mouseLeave()"
+            @focus="mouseOver('updateProducts')"
+            @focusout="mouseLeave()"
+        >
+            Update products
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
 /* eslint-disable */
 
-export default {
+import { defineComponent } from 'vue'
+
+export default defineComponent ({
     name: 'HeaderTabs',
-}
+    data() {
+        return {
+            activeTab: 'favoriteProducts',
+            hoverTab: ''
+        }
+    },
+    methods: {
+        isActive(tab: string): string {
+            if (this.hoverTab == tab) {
+                return 'tab_active'
+            }
+            else if (this.hoverTab == '' && this.activeTab == tab) {
+                return 'tab_active'
+            }
+            return ''
+        },
+        setActivetab(tab: string) {
+            this.activeTab = tab
+        },
+        mouseOver(tab: string) {
+            this.hoverTab = tab
+        },
+        mouseLeave() {
+            this.hoverTab = ''
+        },
+    },
+})
 </script>
 
 <style scoped>
 .tabs {
-    height: 50px;
     background-color: var(--header-color);
 
     margin-bottom: 10px;
@@ -26,11 +86,18 @@ export default {
     align-items: center;
 }
 
-.link {
+.tab {
     color: white;
     text-decoration: none;
-    background-color: rgba(0, 0, 0, 0);
-    padding: 10px;
 
+    padding: 10px 10px;
+    border-bottom: 2px solid var(--header-color);
+
+    transition: all .2s;
+}
+
+.tab_active {
+    color: var(--purple);
+    border-bottom: 2px solid var(--purple);
 }
 </style>
