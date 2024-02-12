8a9a6c7de5cf97a9d9a4597451d619929ea63762
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2024-02-11 21:45:09 -0300

    ✨feat: keep alive for components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         <HeaderTabs />
-        <router-view></router-view>
+        <router-view v-slot="{ Component }">
+            <keep-alive>
+                <component :is="Component" />
+            </keep-alive>
+        </router-view>
+
     </div>
 </template>
 
