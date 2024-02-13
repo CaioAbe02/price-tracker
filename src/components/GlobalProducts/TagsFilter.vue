286f9dfc121a5b0cafb776c4e2732307321f9e6f
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2024-02-12 22:52:31 -0300

    ✨feat: tags filter to products page

---
+++ src/components/GlobalProducts/TagsFilter.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="tags">
+        <div v-for="tag in tags" class="tag" :key="tag">
+            <span class="tag_text">{{ tag }}</span>
+            <font-awesome
+                icon="fa-solid fa-xmark"
+                size=xs
+                class="delete_tag_icon"
+                @click="sendRemoveTag(tag)"
+             />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+    name: 'TagsFilter',
+    props: {
+        tags: {
+            required: true,
+            type: Array as PropType<string[]>,
+        }
+    },
+    methods: {
+        sendRemoveTag(tag: string) {
+            this.$emit('removeTag', tag)
+        }
+    }
+})
+</script>
+
+<style scoped>
+.tags {
+    display: flex;
+    column-gap: 15px;
+}
+
+.tag {
+    display: flex;
+    align-items: center;
+
+    background-color: var(--tag_background);
+
+    padding: 5px 10px;
+    border-radius: 10px;
+}
+
+.tag_text {
+    color: white;
+    font-size: 0.9rem;
+
+    padding-right: 10px;
+}
+
+.delete_tag_icon {
+    color: white;
+
+    cursor: pointer;
+}
+</style>
--- src/views/GlobalProducts.vue
+++ src/views/GlobalProducts.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="products">
         <SearchInput @inputSearchQuery="setSearchQuery" class="search"/>
-        <GlobalProductsTable :products="filteredProducts" @inputSearchQuery="setSearchQuery" v-if="windowWidth > 850"/>
+        <div class="tags_filter" v-if="searchTag.length">
+            <TagsFilter :tags="searchTag" @removeTag="removeSearchTag"/>
+        </div>
+        <GlobalProductsTable :products="filteredProducts" @searchTagQuery="addSearchTag" v-if="windowWidth > 850"/>
         <GlobalProductsCards :products="filteredProducts" v-if="windowWidth <= 850"/>
     </div>
 </template>
@@ -16,19 +19,22 @@ import MyProduct from '@/components/MyProduct.vue';
 import GlobalProductsTable from '@/components/GlobalProducts/GlobalProductsTable.vue'
 import GlobalProductsCards from '@/components/GlobalProducts/GlobalProductsCards.vue'
 import SearchInput from '@/components/SearchInput.vue';
+import TagsFilter from '@/components/GlobalProducts/TagsFilter.vue';
 import IProduct from '@/interfaces/IProduct';
 
 export default defineComponent({
     name: 'Products',
     components: {
-        MyProduct,
-        GlobalProductsTable,
-        GlobalProductsCards,
-        SearchInput,
-    },
+    MyProduct,
+    GlobalProductsTable,
+    GlobalProductsCards,
+    SearchInput,
+    TagsFilter
+},
     data() {
         return {
             searchQuery: '',
+            searchTag: [] as string[],
             windowWidth: window.innerWidth
         }
     },
@@ -52,24 +58,42 @@ export default defineComponent({
         setSearchQuery(searchQuery: string) {
             this.searchQuery = searchQuery
         },
+        addSearchTag(tag: string) {
+            if (!this.searchTag.includes(tag)) {
+                this.searchTag.push(tag)
+            }
+        },
+        removeSearchTag(tag: string) {
+            const index = this.searchTag.indexOf(tag, 0)
+
+            if (index > -1) {
+                this.searchTag.splice(index, 1)
+            }
+        },
         handleResize() {
             this.windowWidth = window.innerWidth;
         }
     },
     computed: {
         filteredProducts(): IProduct[] {
-            if (this.searchQuery == '') {
+            if (this.searchQuery == '' && this.searchTag.length == 0 ) {
                 return this.products
             }
 
             const searchItems = this.searchQuery.toLowerCase().split(' ')
 
-            return this.products.filter(product => {
-                return searchItems.every(item => {
-                    return product.name.toLowerCase().includes(item) ||
-                    product.tags.toLocaleLowerCase().includes(item)
+            return this.products
+                .filter(product => {
+                    return searchItems.every(item => {
+                        return product.name.toLowerCase().includes(item) ||
+                        product.tags.toLocaleLowerCase().includes(item)
+                    })
                 })
-            })
+                .filter(product => {
+                    return this.searchTag.every(tag => {
+                        return product.tags.includes(tag)
+                    })
+                })
         }
     },
 })
@@ -78,5 +102,17 @@ export default defineComponent({
 <style scoped>
 .products {
     margin: 0 150px;
+}
+
+.tags_filter {
+    display: flex;
+    align-items: center;
+
+    margin-bottom: 10px;
+}
+
+.tags_text {
+    color: white;
+    margin-right: 15px;
 }
 </style>
