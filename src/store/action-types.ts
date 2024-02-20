ff38094ac3ffe15c6863d8b0249614c4613d9961
Caio Abe <caioabe68@gmail.com>
2024-02-20 15:01:16 -0300

    🔧change: get products to get data

--- src/store/action-types.ts
+++ src/store/action-types.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
-export const GET_PRODUCTS = "GET_PRODUCTS"
-export const ADD_PRODUCT = "ADD_PRODUCT";
-export const EDIT_PRODUCT = "EDIT_PRODUCT";
-export const UPDATE_PRODUCT_PRICE = "UPDATE_PRODUCT_PRICE";
+export const GET_DATA = "GET_DATA"
+
+export const ADD_PRODUCT = "ADD_PRODUCT"
+export const EDIT_PRODUCT = "EDIT_PRODUCT"
+export const UPDATE_PRODUCT_PRICE = "UPDATE_PRODUCT_PRICE"
