fc85384af0a7a30e4ac83dbca7d2d38ed7cf5f89
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2024-03-03 02:36:21 -0300

    ✨feat: PurpleButton component

---
+++ src/components/Buttons/PurpleButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button>
+        {{ button_text }}
+    </button>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'PurpleButton',
+    props: {
+        button_text: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
+
+<style scoped>
+button {
+    background-color: var(--purple);
+
+    padding: 7px 15px;
+    border-width: 0px;
+    border-radius: 5px;
+
+    cursor: pointer;
+    user-select: none;
+}
+</style>
