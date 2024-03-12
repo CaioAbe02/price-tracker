19e38d72bc5df6934e64611ca6443b9c2d43bd50
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2024-03-12 00:28:32 -0300

    ♻️refactor: myProducts page

--- src/views/MyProducts.vue
+++ src/views/MyProducts.vue
@@ -1,30 +1,19 @@
 <template>
-    <div class="myproducts_background" v-if="products">
-        <div class="products">
-            <div class="product" v-for="product in myProducts" :key="product.id">
-                <div class="move_buttons">
-                    <font-awesome
-                        icon="fa-solid fa-caret-up"
-                        size="lg"
-                        class="product_up"
-                        @click="moveProductUp(product.id)"
-                        v-if="product.id != myProductsIds[0]"
-                    />
-                    <font-awesome
-                        icon="fa-solid fa-caret-down"
-                        size="lg"
-                        class="product_down"
-                        @click="moveProductDown(product.id)"
-                        v-if="product.id != myProductsIds.slice(-1)[0]"
-                    />
-                </div>
-                <MyProduct
-                    :product="product"
-                    @favoriteButton="favoriteProduct"
-                    @moveUp="moveProductUp"
+    <div class="favorite_products">
+        <h1>Favorite products</h1>
+        <draggable
+            v-model="favoriteProducts"
+            item-key="id"
+            v-bind="dragOptions"
+            @end="updateFavoriteProducts"
+            class="products">
+            <template #item="{element}">
+                <FavoriteProductCard
+                    :product="element"
+                    @favoriteButton="unfavoriteProduct"
                 />
-            </div>
-        </div>
+            </template>
+        </draggable>
     </div>
 </template>
 
@@ -32,18 +21,21 @@
 /* eslint-disable */
 
 import { defineComponent, computed } from 'vue';
-import MyProduct from '@/components/MyProduct.vue';
 import { useStore } from '@/store';
+import FavoriteProductCard from '@/components/FavoriteProduct/FavoriteProductCard.vue';
 import IProduct from '@/interfaces/IProduct';
+import draggable from 'vuedraggable';
 
 export default defineComponent({
     name: 'MyProducts',
     components: {
-        MyProduct,
+        draggable,
+        FavoriteProductCard,
     },
     data() {
         return {
             myProductsIds: [] as number[],
+            favoriteProducts: [] as IProduct[]
         }
     },
     setup() {
@@ -58,50 +50,39 @@ export default defineComponent({
         if(localStorage.getItem('myProductsIds')) {
             try {
                 this.myProductsIds = JSON.parse(localStorage.getItem('myProductsIds') || '')
+                this.favoriteProducts = this.setFavoriteProducts()
             }
             catch(e) {
                 localStorage.removeItem('myProductsIds')
             }
         }
     },
+    watch: {
+        products() {
+            this.favoriteProducts = this.setFavoriteProducts()
+        }
+    },
     methods: {
-        favoriteProduct(product_id: number) {
-            const index = this.myProductsIds.indexOf(product_id)
-            if (index != -1) {
-                this.removeProduct(product_id)
+        unfavoriteProduct(product_id: number) {
+            const index = this.favoriteProducts.findIndex(product => product.id === product_id)
+
+            if (index > -1) {
+                this.favoriteProducts.splice(index, 1)
             }
-            else {
-                this.addProduct(product_id)
-            }
-            this.saveProducts()
-        },
-        addProduct(product_id: number) {
-            this.myProductsIds.push(product_id)
-        },
-        removeProduct(product_id: number) {
+
             this.myProductsIds = this.myProductsIds.filter(id => id !== product_id)
-        },
-        moveProductUp(product_id: number) {
-            const index = this.myProductsIds.indexOf(product_id)
-            this.myProductsIds[index] = this.myProductsIds[index - 1]
-            this.myProductsIds[index - 1] = product_id
 
             this.saveProducts()
         },
-        moveProductDown(product_id: number) {
-            const index = this.myProductsIds.indexOf(product_id)
-            this.myProductsIds[index] = this.myProductsIds[index + 1]
-            this.myProductsIds[index + 1] = product_id
-
+        updateFavoriteProducts() {
+            this.myProductsIds = this.favoriteProducts.map(product => product.id)
             this.saveProducts()
         },
         saveProducts() {
             const parsed = JSON.stringify(this.myProductsIds)
             localStorage.setItem('myProductsIds', parsed)
         },
-    },
-    computed: {
-        myProducts(): IProduct[] {
+        setFavoriteProducts() {
             const myProducts = [] as IProduct[]
             const BreakError = "Product found"
             if (this.products.length > 0) {
@@ -124,17 +105,34 @@ export default defineComponent({
             return myProducts
         },
     },
+    computed: {
+        dragOptions() {
+            return {
+                animation: 200,
+                ghostClass: "ghostTag"
+            }
+        },
+    },
 })
 </script>
 
 <style scoped>
+.favorite_products {
+    margin: 0 5%;
+}
+
+h1 {
+    font-size: 32px;
+    color: white;
+
+    margin-bottom: 10px;
+}
+
 .myproducts_background {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
-    background-color: var(--background-color);
 }
 
 .product {
@@ -156,8 +154,36 @@ export default defineComponent({
 }
 
 .products {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 20px;
+}
+
+@media screen and (max-width: 1400px) {
+    .products {
+        grid-template-columns: repeat(4, 1fr);
+    }
+}
+
+@media screen and (max-width: 1100px) {
+    .products {
+        grid-template-columns: repeat(3, 1fr);
+    }
+}
+
+@media screen and (max-width: 830px) {
+    .products {
+        grid-template-columns: repeat(2, 1fr);
+    }
+}
+
+@media screen and (max-width: 570px) {
+    .products {
+        grid-template-columns: repeat(1, 1fr);
+    }
+}
+
+.ghostTag {
+    opacity: 0;
 }
 </style>
