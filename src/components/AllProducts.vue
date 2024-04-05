af06430b145de6fe27616d70e31dc26f2610c29b
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2024-04-05 15:05:00 -0300

    ✨feat: AllProducts component

---
+++ src/components/AllProducts.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>OI</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'AllProducts'
+})
+</script>
+
+<style scoped>
+
+</style>
