293efb4b886e5174eb7552f236758db82e139197
Caio Abe <caioabe68@gmail.com>
2024-05-20 20:40:20 -0300

    ✨feat: header and store

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
+    <HeaderApp />
     <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import HeaderApp from './components/HeaderApp.vue'
+import { productsStore } from './store';
 
 export default defineComponent({
-    name: 'App'
+    name: 'App',
+    components: {
+        HeaderApp,
+    },
+    setup() {
+        const store = productsStore()
+
+        store.getProducts()
+    }
 })
 </script>
 
