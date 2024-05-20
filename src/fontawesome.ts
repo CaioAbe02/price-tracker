3906c86c0fa70b14f7fb8a4f3be1f1d98b26abd4
Caio Abe <caioabe68@gmail.com>
2024-05-20 20:41:30 -0300

    ✨feat: font awesome icons

---
+++ src/fontawesome.ts
@@ -0,0 +1,12 @@
+import {
+    faArrowTrendUp,
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+} from '@fortawesome/free-regular-svg-icons'
+
+const icons = {
+    faArrowTrendUp,
+}
+
+export default icons;
