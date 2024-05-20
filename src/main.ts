ca727a839c84628ac4c25f76fee454bdca279228
Caio Abe <caioabe68@gmail.com>
2024-05-20 20:41:16 -0300

    ✨feat: pinia and font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+import icons from './fontawesome'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add({ ...icons })
 
 import './style.css'
 
+const pinia = createPinia()
+
 createApp(App)
     .use(router)
+    .use(pinia)
+    .component("font-awesome", FontAwesomeIcon)
     .mount('#app')
