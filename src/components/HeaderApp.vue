759348078450350da3ab0966bcf68ec14f4dc0cc
Caio Abe <caioabe68@gmail.com>
2024-05-20 20:41:49 -0300

    ✨feat: HeaderApp component

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="tabs">
+        <router-link
+            to="/products"
+        >
+            Products
+        </router-link>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'HeaderApp'
+})
+</script>
+
+<style scoped>
+
+</style>
