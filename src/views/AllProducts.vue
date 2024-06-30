f43b6b15b5471f09906608b94abd9d2c06563ca6
Caio Abe <caioabe68@gmail.com>
2024-06-29 22:11:35 -0300

    ✨feat: filters

--- src/views/AllProducts.vue
+++ src/views/AllProducts.vue
@@ -5,21 +5,43 @@
             <h2>Filters</h2>
             <section class="filters_checkboxes">
                 <h3>Stores</h3>
-                <CheckboxInput v-model="filter_amazon" label="Amazon" :initiate_checked="true"/>
-                <CheckboxInput v-model="filter_kabum" label="Kabum" :initiate_checked="true"/>
-                <CheckboxInput v-model="filter_mercado_livre" label="Mercado Livre" :initiate_checked="true"/>
+                <CheckboxInput v-model:isChecked="filter_amazon" label="Amazon"/>
+                <CheckboxInput v-model:isChecked="filter_kabum" label="Kabum" :initiate_checked="true"/>
+                <CheckboxInput v-model:isChecked="filter_mercado_livre" label="Mercado Livre" :initiate_checked="true"/>
             </section>
             <section class="filters_checkboxes">
                 <h3>Prices</h3>
-                <CheckboxInput v-model="filter_best_prices" label="Best Prices" :initiate_checked="true"/>
-                <CheckboxInput v-model="filter_low_prices" label="Low Prices" :initiate_checked="true"/>
-                <CheckboxInput v-model="filter_same_prices" label="Same Prices" :initiate_checked="true"/>
-                <CheckboxInput v-model="filter_high_prices" label="High Prices" :initiate_checked="true"/>
-                <CheckboxInput v-model="filter_unavailable_prices" label="Unavailable Prices" :initiate_checked="true"/>
+                <CheckboxInput v-model:isChecked="filter_best_prices" label="Best Prices" :initiate_checked="true"/>
+                <CheckboxInput v-model:isChecked="filter_low_prices" label="Low Prices" :initiate_checked="true"/>
+                <CheckboxInput v-model:isChecked="filter_same_prices" label="Same Prices" :initiate_checked="true"/>
+                <CheckboxInput v-model:isChecked="filter_high_prices" label="High Prices" :initiate_checked="true"/>
+                <CheckboxInput v-model:isChecked="filter_unavailable_prices" label="Unavailable Prices" :initiate_checked="true"/>
+            </section>
+            <section class="filters_checkboxes filter_tags">
+                <h3>Tags</h3>
+                <TextInput placeholder="Search tags..." v-model="search_tags" class="search_tags" />
+                <CheckboxInput
+                    v-for="filtered_tag in filteredTags"
+                    :key="filtered_tag.tag.id"
+                    :label="filtered_tag.tag.name"
+                    v-model:isChecked="filter_tags_bool[filtered_tag.index]"
+                    @click="selectTagToFilter(filtered_tag.tag, filtered_tag.index)"
+                />
             </section>
         </aside>
         <section class="search">
-            <input type="search" placeholder="Search products..." v-model="search_query" class="search_input">
+            <TextInput placeholder="Search products..." v-model="search_query" class="search_input" />
+        </section>
+        <section class="tags" v-if="filter_tags.length > 0">
+            <div class="tag" v-for="tag in filter_tags" :key="tag.id">
+                <span class="tag_name">{{ tag.name }}</span>
+                <font-awesome
+                    icon="fa-solid fa-xmark"
+                    size=sm
+                    class="tag_icon"
+                    @click="removeTagFromFilter(tag, getIndexTag(tag.id))"
+                />
+            </div>
         </section>
         <section class="sort">
             <span>Sort by</span>
@@ -28,31 +50,42 @@
             <SelectInput :options="sort_orders" v-model="sort_order"/>
         </section>
         <section class="products">
-            <ProductCard v-for="product in filteredProducts" :key="product.id" :product="product" />
+            <ProductCard
+                v-for="product in filteredProducts"
+                :key="product.id"
+                :product="product"
+                @emit_tag="addTagToFilter"
+            />
         </section>
     </main>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
-import { productsStore } from '../store';
+import { productsStore, tagsStore } from '../store';
 import { getDiscount, lowestPrice } from '../utils';
 import ProductCard from '../components/AllProducts/ProductCard.vue';
 import SelectInput from '../components/Inputs/SelectInput.vue';
 import CheckboxInput from '../components/Inputs/CheckboxInput.vue';
+import TextInput from '../components/Inputs/TextInput.vue';
 import IProduct from '../interfaces/IProduct';
 import IOption from '../interfaces/IOption';
+import IProductTag from '../interfaces/IProductTag';
+import ITag from '../interfaces/ITag';
+import IFilterdTag from '../interfaces/IFilteredTag';
 
 export default defineComponent({
     name: 'AllProducts',
     components: {
         ProductCard,
         SelectInput,
-        CheckboxInput
+        CheckboxInput,
+        TextInput
     },
     data() {
         return {
             search_query: '',
+            search_tags: '',
             sort_by: 'name',
             sort_order: 'asc',
             filter_amazon: true,
@@ -62,11 +95,13 @@ export default defineComponent({
             filter_low_prices: true,
             filter_same_prices: true,
             filter_high_prices: true,
-            filter_unavailable_prices: true
+            filter_unavailable_prices: true,
+            filter_tags: [] as IProductTag[],
         }
     },
     setup() {
-        const store = productsStore()
+        const products_store = productsStore()
+        const tags_store = tagsStore()
 
         const sort_options: IOption[] = [
             { value: 'name', label: 'Name' },
@@ -80,8 +115,9 @@ export default defineComponent({
         ]
 
         return {
-            store,
-            products: computed(() => store.$state.products),
+            products: computed(() => products_store.$state.products),
+            tags: computed(() => tags_store.$state.tags),
+            filter_tags_bool: computed(() => tags_store.$state.filter_tags),
             sort_options,
             sort_orders
         }
@@ -89,6 +125,53 @@ export default defineComponent({
     methods: {
         isLowestPrice(product: IProduct) : boolean {
             return (lowestPrice(product) === product.new_prices.slice(-1)[0])
+        },
+        addTagToFilter(tag: IProductTag, index: number) {
+            if (!this.filter_tags.some(filter_tag => filter_tag.id === tag.id)) {
+                this.filter_tags.push(tag)
+
+                if (index === -1) {
+                    this.filter_tags_bool[this.getIndexTag(tag.id)] = true
+                }
+                else {
+                    this.filter_tags_bool[index] = true
+                }
+                return 1
+            }
+            return -1
+        },
+        removeTagFromFilter(tag: IProductTag, index: number) {
+            const index_filter_tag = this.filter_tags.findIndex(filter_tag => filter_tag.id === tag.id)
+
+            if (index_filter_tag !== -1) {
+                this.filter_tags.splice(index_filter_tag, 1)
+
+                if (index === -1) {
+                    this.filter_tags_bool[this.getIndexTag(tag.id)] = false
+                }
+                else {
+                    this.filter_tags_bool[index] = false
+                }
+                return 1
+            }
+            return -1
+        },
+        selectTagToFilter(tag: IProductTag, index: number = -1) {
+            const result = this.addTagToFilter(tag, index)
+
+            if (result === -1) {
+                this.removeTagFromFilter(tag, index)
+            }
+        },
+        toIProductTag(tag: ITag) {
+            const new_tag: IProductTag = {id: tag.id, name: tag.name}
+            return new_tag
+        },
+        isTagInFilter(tag: IProductTag): Boolean {
+            return this.filter_tags.some(filter_tag => filter_tag.id === tag.id)
+        },
+        getIndexTag(tag_id: number) {
+            return this.tags.findIndex(tag => tag.id === tag_id)
         }
     },
     computed: {
@@ -141,7 +224,36 @@ export default defineComponent({
                 products = products.filter(product => product.name.toLowerCase().includes(this.search_query.toLowerCase()))
             }
 
+            if (this.filter_tags.length !== 0) {
+                products = products.filter(product => {
+                    return this.filter_tags.every(filterTag =>
+                        product.tags.some(productTag => productTag.id === filterTag.id)
+                    )
+                })
+            }
+
             return products
+        },
+        filteredTags(): IFilterdTag[] {
+            let filtered_tags: IFilterdTag[] = []
+
+            for (let i in this.tags) {
+                const tag = this.tags[i]
+                const filterd_tag: IFilterdTag = {
+                    tag: {
+                        id: tag.id,
+                        name: tag.name
+                    },
+                    index: parseInt(i)
+                }
+                filtered_tags.push(filterd_tag)
+            }
+
+            if (this.search_tags === '') {
+                return filtered_tags
+            }
+
+            return filtered_tags.filter(filtered_tag => filtered_tag.tag.name.toLowerCase().includes(this.search_tags.toLowerCase()))
         }
     }
 })
@@ -153,10 +265,11 @@ export default defineComponent({
         grid-template-areas:
             'title title'
             'filters search'
+            'filters tags'
             'filters sort'
             'filters products';
         grid-template-columns: 25% 1fr;
-        grid-template-rows: auto 60px 40px auto;
+        grid-template-rows: auto 60px minmax(0, auto) 40px auto;
 
         margin: 0 5%;
     }
@@ -197,43 +310,54 @@ export default defineComponent({
             border-bottom: 1px solid var(--product-tag-separator);
         }
 
+            .filter_tags {
+                max-height: 400px;
+                overflow-y: scroll;
+            }
+
+                .search_tags {
+                    padding: 12px;
+                    margin-bottom: 4px;
+
+                    font-size: 14px;
+                }
+
     .search {
         grid-area: search;
 
         margin-bottom: 8px;
     }
 
-        .search_input {
-            width: 100%;
+    .tags {
+        grid-area: tags;
 
-            font-size: 16px;
-            color: var(--product-tag-color);
+        display: flex;
+        column-gap: 4px;
+
+        margin-bottom: 8px;
+    }
+
+        .tag {
+            display: flex;
+            align-items: center;
+            column-gap: 4px;
 
             background-color: var(--input-color);
 
-            padding: 16px;
-            border: 1px solid var(--input-color);
-            border-radius: 8px;
-            outline: none;
-
-            &:focus {
-                border: 1px solid var(--white);
-
-                transition: border 0.2s ease;
-            }
-
-            &::placeholder {
-                transition: color 0.2s ease;
-            }
-
-            &:hover::placeholder {
-                color: var(--white);
-            }
-
-            &:focus::placeholder {
-                color: var(--white);
-            }
+            padding: 4px 8px;
+            border-radius: 16px;
         }
+
+            .tag_name {
+                color: var(--product-tag-color);
+                font-size: 14px;
+            }
+
+            .tag_icon {
+                color: var(--product-tag-color);
+
+                cursor: pointer;
+            }
 
     .sort {
         grid-area: sort;
