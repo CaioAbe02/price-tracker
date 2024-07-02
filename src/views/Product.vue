cbd2243393c0962b60dfc764a2ea3d9d43824d8a
Caio Abe <caioabe68@gmail.com>
2024-07-02 12:09:39 -0300

    ✨feat: Product view

---
+++ src/views/Product.vue
@@ -0,0 +1,31 @@
+<template>
+    <main v-if="product">
+        {{ product.name }}
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import { productsStore } from '../store';
+
+export default defineComponent({
+    name: 'Product',
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
+    },
+    setup(props) {
+        const products_store = productsStore()
+
+        return {
+            product: computed(() => products_store.$state.products.filter((product) => product.id === parseInt(props.id))[0]),
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
